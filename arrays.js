11b705f3d92ec14ac140afacf77789e8fe3f9887
Kelli Budd <buddkelli1@gmail.com>
2020-05-19 18:52:21 -0500

    Implement JS function that prints a list of every other item when given a list of items

--- arrays.js
+++ arrays.js
@@ -3,6 +3,7 @@
 
 // 1. printIndices
 function printIndices(items) {
+
     for (const idx in items) {
         console.log(`${items[idx]} ${idx}`);
     }
@@ -10,8 +11,34 @@ function printIndices(items) {
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+
+    const result = []
+
+    for (const idx in items) {
+        if (idx % 2 === 0) {
+            result.push(items[idx]);
+        }
+    }
+
+    console.log(result)
 }
+
+// def every_other_item(items):
+//     """Print a list of every other item in `items`
+
+//     Start with index 0.
+
+//     Arguments:
+//         items (list)
+//     """
+
+//     result = []
+
+//     for i in range(len(items)):
+//         if i % 2 == 0:
+//             result.append(items[i])
+
+//     print(result)
 
 
 // 3. smallestNItems
