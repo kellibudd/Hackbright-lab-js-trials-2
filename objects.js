c412b999dd100387515d453cd9676d945be391d7
Kelli Budd <buddkelli1@gmail.com>
2020-05-22 04:28:01 +0000

    Complete object functions in JS

--- objects.js
+++ objects.js
@@ -3,11 +3,36 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  
+  const wordCounts = {};
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+  return wordCounts;
 }
 
+console.log(countWords('Gipsy moon loves Britt Britt'))
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  
+  const melonPrice = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }
+
+  if (!melonPrice[price]) {
+    return;
+  } else {
+    return melonPrice[price].sort();
+  }
 }
+
+console.log(getMelonsAtPrice(3.25))
